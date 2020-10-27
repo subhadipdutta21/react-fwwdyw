52b828c14491d19d5c4189553f5653b5c55442b1
subhadipdutta21 <dsubhadip21@gmail.com>
2020-10-27 18:28:10 +0530

    1 file modified

--- src/Components/TableFilter.js
+++ src/Components/TableFilter.js
@@ -1,17 +1,48 @@
-import React from "react";
-import {
-  EditOutlined,
-  HeartFilled,
-  MessageOutlined,
-  PlusOutlined,
-  SettingOutlined
-} from "@ant-design/icons";
+import React, { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { Card, Select, Input } from "antd";
+const { Option } = Select;
 
 const TableFilter = ({}) => {
+  const [showAddBtn, setShowAddBtn] = useState(true);
+
   return (
     <>
       <h4>Filters </h4>
-      <PlusOutlined /> Add Filter
+      <Card style={{ width: "60%", textAlign: "center" }}>
+        {showAddBtn ? (
+          <>
+            <PlusOutlined onClick={() => setShowAddBtn(false)} /> Add Filter
+          </>
+        ) : (
+          <div style={{ display: "flex" }}>
+            <p> Where </p>
+            <Select
+              defaultValue="lucy"
+              style={{ width: 120 }}
+              onChange={val => console.log(val)}
+            >
+              <Option value="col 1">"col 1"</Option>
+              <Option value="col 2">"col 2"</Option>
+              <Option value="col 3">"col 3"</Option>
+            </Select>
+
+            <Select
+              defaultValue="Contains"
+              style={{ width: 120 }}
+              onChange={val => console.log(val)}
+            >
+              <Option value="col 1">"Contains"</Option>
+              <Option value="col 2">{">="}</Option>
+              <Option value="col 3">{"<="}</Option>
+              <Option value="col 4">{"Equals"}</Option>
+            </Select>
+            <Input.Group size="medium">
+              <Input defaultValue="0571" style={{ width: "20%" }} />
+            </Input.Group>
+          </div>
+        )}
+      </Card>
     </>
   );
 };
