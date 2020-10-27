d4618bd4fb7bc368648d8a420c80537784885b82
subhadipdutta21 <dsubhadip21@gmail.com>
2020-10-27 19:11:29 +0530

    1 file modified

--- src/Components/TableFilter.js
+++ src/Components/TableFilter.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
-import { Card, Select, Input } from "antd";
+import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Card, Select, Input, Row, Col } from "antd";
 const { Option } = Select;
 
 const TableFilter = ({}) => {
   const [showAddBtn, setShowAddBtn] = useState(true);
+
+  const columnVals = [
+    { name: "Name" },
+    { screen_name: "Screen Name" },
+    { followers_count: "Followers Count" },
+    { following_count: "Following Count" },
+    { location: "Location" },
+    { verified: "Verified" }
+  ];
+
+  const filterVals = [
+    { CONTAINS: "Contains" },
+    { GTE: ">=" },
+    { LTE: "<=" },
+    { EQ: "Equals" }
+  ];
 
   return (
     <>
@@ -15,32 +31,44 @@ const TableFilter = ({}) => {
             <PlusOutlined onClick={() => setShowAddBtn(false)} /> Add Filter
           </>
         ) : (
-          <div style={{ display: "flex" }}>
-            <p> Where </p>
-            <Select
-              defaultValue="lucy"
-              style={{ width: 120 }}
-              onChange={val => console.log(val)}
-            >
-              <Option value="col 1">"col 1"</Option>
-              <Option value="col 2">"col 2"</Option>
-              <Option value="col 3">"col 3"</Option>
-            </Select>
+          <Row justify="center">
+            <Col span={4}>
+              <p> Where </p>
+            </Col>
+            <Col span={4}>
+              <Select style={{ width: 120 }} onChange={val => console.log(val)}>
+                {columnVals.map(itm => (
+                  <Option value={Object.keys(itm)[0]}>
+                    {Object.values(itm)[0]}
+                  </Option>
+                ))}
+              </Select>
+            </Col>
 
-            <Select
-              defaultValue="Contains"
-              style={{ width: 120 }}
-              onChange={val => console.log(val)}
-            >
-              <Option value="col 1">"Contains"</Option>
-              <Option value="col 2">{">="}</Option>
-              <Option value="col 3">{"<="}</Option>
-              <Option value="col 4">{"Equals"}</Option>
-            </Select>
-            <Input.Group size="medium">
-              <Input defaultValue="0571" style={{ width: "20%" }} />
-            </Input.Group>
-          </div>
+            <Col span={4}>
+              <Select
+                placeholder=""
+                style={{ width: 120 }}
+                onChange={val => console.log(val)}
+              >
+                {filterVals.map(itm => (
+                  <Option value={Object.keys(itm)[0]}>
+                    {Object.values(itm)[0]}
+                  </Option>
+                ))}
+              </Select>
+            </Col>
+
+            <Col span={4}>
+              <Input.Group size="medium">
+                <Input placeholder="value" />
+              </Input.Group>
+            </Col>
+
+            <Col span={4}>
+              <DeleteOutlined onClick={() => console.log("delete this")} />
+            </Col>
+          </Row>
         )}
       </Card>
     </>
